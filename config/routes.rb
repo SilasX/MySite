95ff51b733d6a9a177bd5a5dcc237a16f3ba82cd
Silas Barta <sbarta@gmail.com>
2012-04-25 17:15:06 -0700

    added staticpages resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails3DeviseRspecCucumber::Application.routes.draw do
 #  authenticated :user do
 #    root :to => 'home#index'
 #  end
-  root :to => "users#index"
+  get "/intro" => "staticpages#intro"
+  root :to => "staticpages#intro"
   devise_for :users
   resources :users, :only => [:show, :index]
 end
