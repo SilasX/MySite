c4822d3ae1efe766011e22fb01ff7e479a149a43
Silas Barta <sbarta@gmail.com>
2012-04-28 00:02:40 -0700

    another precompile

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Rails3DeviseRspecCucumber::Application.initialize!
-
-config.assets.precompile += %w( *.css )
